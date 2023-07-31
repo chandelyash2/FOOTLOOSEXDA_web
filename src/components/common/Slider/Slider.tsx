5563f45721c426c340a978d615c4a9dabef96f0f
Yash Chandel <yashchandel@192.168.1.12>
2023-07-31 14:00:56 +0530

    add custom arrow for slider

--- src/components/common/Slider/Slider.tsx
+++ src/components/common/Slider/Slider.tsx
@@ -2,6 +2,8 @@ import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import Container from '../Container'
+import { LeftArrow } from '../icons/LeftArrow'
+import { RightArrow } from '../icons/RightArrow'
 
 const imgArr = [
   {
@@ -19,7 +21,25 @@ const imgArr = [
   },
   { id: 5, name: '/img/Kheerganga.png' },
 ]
-
+function Arrow(props: any) {
+  let className = props.type === 'next' ? 'nextArrow' : 'prevArrow'
+  className += ' arrow'
+  const char =
+    props.type === 'next' ? (
+      <div className="absolute cursor-pointer top-[45%] right-1 w-10 h-10 bg-secondary flex justify-center items-center rounded-full ">
+        <RightArrow />
+      </div>
+    ) : (
+      <div className="absolute cursor-pointer top-[45%] left-1  w-10 h-10 bg-secondary flex justify-center items-center rounded-full ">
+        <LeftArrow />
+      </div>
+    )
+  return (
+    <span className={className} onClick={props.onClick}>
+      {char}
+    </span>
+  )
+}
 
 export const SliderComponent = () => {
   const settings = {
@@ -28,13 +48,34 @@ export const SliderComponent = () => {
     centerMode: true,
     slidesToShow: 3,
     speed: 500,
-    centerPadding: '30px'
+    centerPadding: '30px',
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: true,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
   }
   return (
     <Container>
-      <Slider {...settings}>
+      <Slider
+        {...settings}
+        nextArrow={<Arrow type="next" />}
+        prevArrow={<Arrow type="prev" />}
+      >
         {imgArr.map((item, i) => (
-          <div key={i} className="p-8">
+          <div key={i} className="p-8 lg:p-8">
             <div
               className="sliderwarpper relative flex items-end rounded-[30px] h-[463px] bg-cover text-center before:rounded-[30px] before:content-[''] before:bg-gradient2 before:absolute before:top-0 before:w-full before:h-full "
               style={{ backgroundImage: `url(${item.name})` }}
